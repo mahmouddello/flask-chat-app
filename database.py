fee1b7440e06bf168a5653f747e01bebedc92807
Mahmoud Dello <mahmoddello68@gmail.com>
2023-09-15 17:44:52 +0300

    Add type hints and docstrings for the existing functions in database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 from bson import ObjectId
-from flask import jsonify
+from flask import jsonify, Response
 from flask_login import current_user
 from pymongo.mongo_client import MongoClient
 from werkzeug.security import generate_password_hash
@@ -45,34 +45,27 @@ def save_user(username: str, email: str, password: str) -> None:
     })
 
 
-def get_room(room_id):
+# Room Operations
+
+def get_room(room_id: ObjectId) -> dict:
     """Find a room by ```room_id```, auto generated id in the mongo db is saved in `Binary text JSON Format`,
     so we need to convert our string to a format of BSON to match with the ```_id``` attribute."""
     return rooms_collection.find_one({'_id': ObjectId(room_id)})
 
 
-def get_rooms_for_user(username):
+def get_rooms_for_user(username: str) -> list:
+    """Returns list of rooms for the current signed-in user."""
     return list(room_members_collection.find({'_id.username': username}))
 
 
-def get_messages(room_id):
-    return list(messages_collection.find({"room_id": room_id}))
-
-
-def save_message(room_id, text, sender):
-    messages_collection.insert_one({
-        "room_id": room_id,
-        "text": text,
-        "sender": sender,
-        "create_at": datetime.now()
-    })
-
-
-def get_room_members(room_id):
+def get_room_members(room_id: ObjectId) -> list:
+    """Returns a list of members for a room."""
     return list(room_members_collection.find({'_id.room_id': ObjectId(room_id)}))
 
 
-def join_room_member(room_id, room_name, username, added_by="Himself", is_room_admin=False):
+def join_room_member(room_id: ObjectId, room_name: str, username: str, added_by="Himself",
+                     is_room_admin=False) -> Response:
+    """Responsible for joining room functionality by room_id, handling this operation on join room modal."""
     room_members_collection.insert_one(
         {'_id': {'room_id': ObjectId(room_id), 'username': username}, 'room_name': room_name, 'added_by': added_by,
          'added_at': datetime.now(), 'is_room_admin': is_room_admin})
@@ -80,3 +73,20 @@ def join_room_member(room_id, room_name, username, added_by="Himself", is_room_a
     room_members = [member["_id"]["username"] for member in room_members]
     if current_user.username in room_members:
         return jsonify({"status": "Joined"})
+
+
+# Messages
+
+def get_messages(room_id: ObjectId) -> list:
+    """Returns list of messages for the selected room."""
+    return list(messages_collection.find({"room_id": room_id}))
+
+
+def save_message(room_id: ObjectId, text: str, sender: str):
+    """Save message to the database after emitting event of SocketIO."""
+    messages_collection.insert_one({
+        "room_id": room_id,
+        "text": text,
+        "sender": sender,
+        "create_at": datetime.now()
+    })
